caeef330f1a249967979425d22edbae0cfda489d
David Albert <davidbalbert@gmail.com>
2016-02-08 14:17:44 -0500

    Controller support

--- config.ru
+++ config.ru
@@ -1,9 +1,24 @@
 require 'no_framework'
 
+class BlogController < NoFramework::Controller
+  def index
+    <<~END
+    1. First post
+    2. Second post
+    3. getting old
+    END
+  end
+
+  def print_params
+    params.to_s
+  end
+end
+
 MyApp = NoFramework::Application.new do
   root ->(env) { [200, {}, ['Hello, world!']] }
   get '/foo', ->(env) { [200, {}, ['foo']] }
-  get '/bar', ->(env) { [200, {}, ['bar']] }
+  get '/blog', to: 'blog#index'
+  get '/params', to: 'blog#print_params'
 end
 
 run MyApp
--- lib/no_framework.rb
+++ lib/no_framework.rb
@@ -41,23 +41,34 @@ module NoFramework
       end
 
       def get(path, handler)
-        @routes[[normalize(path), 'GET']] = handler
+        @routes[[normalize(path), 'GET']] = process_handler(handler)
       end
 
       def post(path, handler)
-        @routes[[normalize(path), 'POST']] = handler
+        @routes[[normalize(path), 'POST']] = process_handler(handler)
       end
 
       def put(path, handler)
-        @routes[[normalize(path), 'PUT']] = handler
+        @routes[[normalize(path), 'PUT']] = process_handler(handler)
       end
 
       def delete(path, handler)
-        @routes[[normalize(path), 'DELETE']] = handler
+        @routes[[normalize(path), 'DELETE']] = process_handler(handler)
       end
 
       def patch(path, handler)
-        @routes[[normalize(path), 'PATCH']] = handler
+        @routes[[normalize(path), 'PATCH']] = process_handler(handler)
+      end
+
+      def process_handler(handler)
+        if handler.is_a?(Hash)
+          controller, action = handler[:to].split('#')
+          klass = Object.const_get(controller.capitalize + 'Controller')
+
+          ->(env) { klass.new(Rack::Request.new(env)).action(action).call(env) }
+        else
+          handler
+        end
       end
 
       def to_h
@@ -85,6 +96,20 @@ module NoFramework
 
     def add_routes(dsl)
       @routes.merge!(dsl.to_h)
+    end
+  end
+
+  class Controller
+    def initialize(req)
+      @req = req
+    end
+
+    def action(name)
+      ->(env) { [200, {}, [send(name)]] }
+    end
+
+    def params
+      @req.params
     end
   end
 end
